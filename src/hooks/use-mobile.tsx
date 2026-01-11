33a2ce8c02a1daadfb75d1e971211c99b9f3330a
Raunaq Adlakha <raunaq.adlakha@gmail.com>
2026-01-11 14:11:44 +0000

    Console Error
    A tree hydrated but some attributes of the server rendere

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -5,19 +5,22 @@ import * as React from "react"
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState(false)
+  const [isMobile, setIsMobile] = React.useState(false);
 
   React.useEffect(() => {
     const checkDevice = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
-    // Check on mount
-    checkDevice()
-    // Add resize listener
-    window.addEventListener("resize", checkDevice)
-    // Cleanup
-    return () => window.removeEventListener("resize", checkDevice)
-  }, [])
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    };
 
-  return isMobile
+    // Check on mount (after hydration)
+    checkDevice();
+
+    // Add resize listener
+    window.addEventListener("resize", checkDevice);
+
+    // Cleanup
+    return () => window.removeEventListener("resize", checkDevice);
+  }, []);
+
+  return isMobile;
 }
